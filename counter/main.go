17941f0715b8a9f48934aed74e2ccb4a68094ad2
Adit0507 <adi05072002@gmail.com>
2025-01-26 15:07:58 +0530

    pushing results periodically to db

--- counter/main.go
+++ counter/main.go
@@ -5,18 +5,54 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
+	"time"
 
 	"github.com/nsqio/go-nsq"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var fatalErr error
+
+const updateDuration = 1 * time.Second
 
 func fatal(e error) {
 	fmt.Println(e)
 	flag.PrintDefaults()
 	fatalErr = e
+}
+
+// will push the results to DB
+func doCount(countsLock *sync.Mutex, counts *map[string]int, pollData *mgo.Collection) {
+	countsLock.Lock()
+	defer countsLock.Unlock()
+
+	if len(*counts) == 0 {
+		log.Println("No new votes, skipping database update")
+		return
+	}
+
+	log.Println("Updating databse..")
+	log.Println(*counts)
+
+	ok := true
+	for option, count := range *counts {
+		sel := bson.M{"options": bson.M{"$in": []string{option}}}
+		up := bson.M{"$inc": bson.M{"results." + option: count}}
+
+		if _, err := pollData.UpdateAll(sel, up); err != nil {
+			log.Println("failed to update: ", err)
+			ok = false
+		}
+	}
+
+	if ok {
+		log.Println("Finished updating DB....")
+		*counts = nil //reset counts
+	}
 }
 
 func main() {
@@ -70,4 +106,19 @@ func main() {
 		return
 	}
 
+	ticker := time.NewTicker(updateDuration)
+	termChan := make(chan os.Signal, 1)
+	signal.Notify(termChan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP)
+	for {
+		select {
+		case <-ticker.C:
+			doCount(&countsLock, &counts, pollData)
+		case <-termChan:
+			ticker.Stop()
+			q.Stop()
+		case <-q.StopChan:
+			// finished
+			return
+		}
+	}
 }
