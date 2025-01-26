1ef7260f415e11e6e7a704101081a97838361736
Adit0507 <adi05072002@gmail.com>
2025-01-26 18:10:40 +0530

    handler functions

---
+++ api/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"net/http"
+
+	"gopkg.in/mgo.v2"
+)
+
+type contextKey struct {
+	name string
+}
+
+var contextKeyAPIKey = &contextKey{"api-key"}
+
+func APIKey(ctx context.Context) (string, bool) {
+	key, ok := ctx.Value(contextKeyAPIKey).(string)
+
+	return key, ok
+}
+
+func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		key := r.URL.Query().Get("key")
+		if !isValidAPIKey(key) {
+			respondErr(w, r, http.StatusUnauthorized, "invalid API key")
+			return
+		}
+		ctx := context.WithValue(r.Context(),
+			contextKeyAPIKey, key)
+		fn(w, r.WithContext(ctx))
+	}
+}
+
+func isValidAPIKey(key string) bool {
+	return key == "abc123"
+}
+
+func withCORS(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Expose-Headers", "Location")
+		fn(w, r)
+	}
+}
+
+type Server struct {	//encapsulates all dependencies for handlers
+	db *mgo.Session
+}
+
+func main() {}
