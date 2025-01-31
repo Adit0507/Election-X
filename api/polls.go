96fbbc84b3cc78fa09bfd70d94a6767eb3bdff93
Adit0507 <adi05072002@gmail.com>
2025-01-31 23:07:44 +0530

    reading polls

--- api/polls.go
+++ api/polls.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"net/http"
 
+	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
 
@@ -35,9 +36,30 @@ func (s *Server) handlePolls(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) handlePollsGet(w http.ResponseWriter, r*http.Request) {
-	respondErr(w, r, http.StatusInternalServerError)
-	errors.New("not implemented")
+	session := s.db.Copy()	//creatin copy of DB session that will allow us to interact with Mongo
+	defer session.Close()
+	
+	c := session.DB("ballots").C("polls")
+	
+	var q *mgo.Query	
+	p := NewPath(r.URL.Path)
+	if p.HasID() {
+		// gets specific poll
+		q = c.FindId(bson.ObjectIdHex(p.ID))
+	} else {
+		q = c.Find(nil)	//get all polls
+	}
+
+	var res []*poll
+	if err := q.All(&res); err != nil {
+		respondErr(w, r, http.StatusInternalServerError, err)
+		return
+	}
+
+	respond(w, r, http.StatusOK, &res)
 }
+
+
 func (s *Server) handlePollsPost(w http.ResponseWriter, r*http.Request) {
 	respondErr(w, r, http.StatusInternalServerError)
 	errors.New("not implemented")
